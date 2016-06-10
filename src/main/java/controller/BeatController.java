a8c6fd598a71fdd9fcc1f561aa12612fce8af979
Christian Nicolaide <cnicolaide@gmail.com>
2016-06-10 18:52:10 -0300

    Quito plugin checkstyle de gradlew y agrego otros para eclipse, idea, y demas

--- src/main/java/controller/BeatController.java
+++ src/main/java/controller/BeatController.java
@@ -4,42 +4,42 @@ import model.BeatModelInterface;
 import view.DJView;
 
 public class BeatController implements ControllerInterface {
-	BeatModelInterface model;
-	DJView view;
+  BeatModelInterface model;
+  DJView view;
 
-	public BeatController(BeatModelInterface model) {
-		this.model = model;
-		view = new DJView(this, model);
-		view.createView();
-		view.createControls();
-		view.disableStopMenuItem();
-		view.enableStartMenuItem();
-		model.initialize();
-	}
+  public BeatController(BeatModelInterface model) {
+    this.model = model;
+    view = new DJView(this, model);
+    view.createView();
+    view.createControls();
+    view.disableStopMenuItem();
+    view.enableStartMenuItem();
+    model.initialize();
+  }
 
-	public void start() {
-		model.on();
-		view.disableStartMenuItem();
-		view.enableStopMenuItem();
-	}
+  public void start() {
+    model.on();
+    view.disableStartMenuItem();
+    view.enableStopMenuItem();
+  }
 
-	public void stop() {
-		model.off();
-		view.disableStopMenuItem();
-		view.enableStartMenuItem();
-	}
+  public void stop() {
+    model.off();
+    view.disableStopMenuItem();
+    view.enableStartMenuItem();
+  }
 
-	public void increaseBPM() {
-		int bpm = model.getBPM();
-		model.setBPM(bpm + 1);
-	}
+  public void increaseBPM() {
+    int bpm = model.getBPM();
+    model.setBPM(bpm + 1);
+  }
 
-	public void decreaseBPM() {
-		int bpm = model.getBPM();
-		model.setBPM(bpm - 1);
-	}
+  public void decreaseBPM() {
+    int bpm = model.getBPM();
+    model.setBPM(bpm - 1);
+  }
 
-	public void setBPM(int bpm) {
-		model.setBPM(bpm);
-	}
+  public void setBPM(int bpm) {
+    model.setBPM(bpm);
+  }
 }
